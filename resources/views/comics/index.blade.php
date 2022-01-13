68df4395034654d869bb8c3019815745c70d40e4
20Federico <fefe.molino@gmail.com>
2022-01-13 18:21:31 +0100

    added layout and style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,1 +1,10 @@
-@dump($data)
+@extends('layouts.default')
+
+@section('page_title', 'Comics | Index')
+
+@section('main_content')
+  @dump($data)
+@endsection
+
+
+
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,32 @@
+@php
+
+$navLinks= [
+        ["linkName"=> 'CHARACTERS', "route_name"=> 'characters.index'],
+        ["linkName"=> 'COMICS', "route_name"=> 'comics.index'],
+        ["linkName"=> 'MOVIES', "route_name"=> 'movies.index'],
+        ["linkName"=> 'TV', "route_name"=> 'tv.index'],
+        ["linkName"=> 'GAMES', "route_name"=> 'games.index'],
+        ["linkName"=> 'COLLECTIBLES', "route_name"=> 'collectibles.index'],
+        ["linkName"=> 'VIDEOS', "route_name"=> 'videos.index'],
+        ["linkName"=> 'FANS', "route_name"=> 'fans.index'],
+        ["linkName"=> 'NEWS', "route_name"=> 'news.index'],
+        ["linkName"=> 'SHOP', "route_name"=> 'shop.index']
+];
+$activeRouteName = Request::route()->getName();
+@endphp
+
+<header>
+  <nav class="container">
+    <img src="{{asset('img/dc-logo.png')}}" alt="dc logo">
+    <ul>
+      @foreach ($navLinks as $link)
+      <li>
+        <a class="{{$link['linkName'] === 'SHOP' ? 'dropdown-toggle' : ''}} {{$link['route_name'] === $activeRouteName ? 'active' : '' }}" href="#">{{ $link['linkName'] }}</a>
+      </li>
+      @endforeach
+    </ul>
+  </nav>
+
+  <div class="banner">
+  </div>
+</header>
